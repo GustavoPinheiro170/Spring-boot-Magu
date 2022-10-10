12cdaa8758bca424e1fe137a6244e240c0e35008
Gabriel Campos <gpcampos@bbmapfre.com.br>
2022-10-10 17:34:03 -0300

    * Add profile DEV to aplication * Add Handle Exception for search by email

--- src/main/java/br/com/magu/magu/service/impl/clientServiceImpl.java
+++ src/main/java/br/com/magu/magu/service/impl/clientServiceImpl.java
@@ -1,32 +1,45 @@
 package br.com.magu.magu.service.impl;
 
-import br.com.magu.magu.models.Clients.ClientRequest;
 import br.com.magu.magu.models.Clients.ClientResponseDTO;
 import br.com.magu.magu.service.ClientService;
 import br.com.magu.magu.utils.IntegrationUtil;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.inject.internal.ErrorsException;
 import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 
 
 public class clientServiceImpl implements ClientService {
-    private static final     TypeReference<ClientResponseDTO> clientResponse = new TypeReference<>() {};
+    private static final Logger logger = LoggerFactory.getLogger(clientServiceImpl.class);
+    private static final  TypeReference<ClientResponseDTO> clientResponse = new TypeReference<>() {};
+
+    @Value("${url.mp}")
+    private String urlMp;
+
     @Autowired private IntegrationUtil integrationUtil;
+
     @SneakyThrows
     @Override
-    public ClientResponseDTO getCustomerClient(ClientRequest request) throws IOException, ErrorsException {
+    public ClientResponseDTO getCustomerClient(String email) throws IOException, ErrorsException {
+
+        logger.info("Calling Client By Email MP");
+
         ResponseEntity<ClientResponseDTO> response =
                     integrationUtil.getRestCall(
-                            "https://api.mercadopago.com/v1/customers/search?email="+request.getEmail() ,
+                            urlMp + "/v1/customers/search?email=" + email ,
                             "GET",
                             null,
                             clientResponse);
-        return response.getBody();
-
+        if(response.getBody() != null && response.getBody().getResults().size() != 0){
+            return response.getBody();
+        }
+        return null ;
 
     }
 }
